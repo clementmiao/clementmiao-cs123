558d8a91486494118b3d42bb9df8e61fd8b30b02
Clement Miao <clementmiao@gmail.com>
2014-05-04 16:25:52 -0500

    refactoring

--- aggregate.py
+++ aggregate.py
@@ -25,93 +25,32 @@ def produce_results():
             for m in l:
                 pitcher = m['@pitcher']
                 pitch = m['pitch']
+                pitch_possibilities = [['FF','FA'],['FT'],['FC'],['SI','FS'],['SF'],['SL'],['CH'],['CB','CU'],['KC'],['KN'],['EP']]
                 if pitcher not in dic:
                     dic[pitcher] = {}
                     dic[pitcher]['pitch_type'] = {}
                     pitch_type = dic[pitcher]['pitch_type']
-                    pitch_type['FF'] = 0
-                    pitch_type['FT'] = 0
-                    pitch_type['FC'] = 0
-                    pitch_type['SI'] = 0
-                    pitch_type['SF'] = 0
-                    pitch_type['SL'] = 0
-                    pitch_type['CH'] = 0
-                    pitch_type['CB'] = 0
-                    pitch_type['KC'] = 0
-                    pitch_type['KN'] = 0
-                    pitch_type['EP'] = 0
+                    # pitch_type_list = ['FF','FT','FC','SI','SF','SL','CH','CB','KC','KN','EP']
+                    for poss in pitch_possibilities:
+                        pitch_type[poss[0]] = 0
                     pitch_type['total'] = 0.0
+                # pitch_possibilities = [['FF','FA'],['FT'],['FC'],['SI','FS'],['SF'],['SL'],['CH'],['CB','CU'],['KC'],['KN'],['EP']]
                 if isinstance(pitch, dict):
                     pt = pitch['@pitch_type']
-                    if pt == 'FF' or pt == 'FA':
-                        pitch_type['FF'] += 1
-                        pitch_type['total'] += 1
-                    elif pt == 'FT':
-                        pitch_type['FT'] += 1
-                        pitch_type['total'] += 1
-                    elif pt == 'FC':
-                        pitch_type['FC'] += 1
-                        pitch_type['total'] += 1
-                    elif pt == 'SI' or pt == 'FS':
-                        pitch_type['SI'] += 1
-                        pitch_type['total'] += 1
-                    elif pt == 'SF':
-                        pitch_type['SF'] += 1
-                        pitch_type['total'] += 1
-                    elif pt == 'SL':
-                        pitch_type['SL'] += 1
-                        pitch_type['total'] += 1
-                    elif pt == 'CH':
-                        pitch_type['CH'] += 1
-                        pitch_type['total'] += 1
-                    elif pt == 'CB' or pt == 'CU':
-                        pitch_type['CB'] += 1
-                        pitch_type['total'] += 1
-                    elif pt == 'KC':
-                        pitch_type['KC'] += 1
-                        pitch_type['total'] += 1
-                    elif pt == 'KN':
-                        pitch_type['KN'] += 1
-                        pitch_type['total'] += 1
-                    elif pt == 'FT':
-                        pitch_type['EP'] += 1
-                        pitch_type['total'] += 1
+                    for poss in pitch_possibilities:
+                        for t in poss:
+                            if pt == t:
+                                pitch_type[poss[0]] += 1
+                                pitch_type['total'] += 1
                 else:    
                     for pi in pitch:
                         pt = pi['@pitch_type']
-                        if pt == 'FF' or pt == 'FA':
-                            pitch_type['FF'] += 1
-                            pitch_type['total'] += 1
-                        elif pt == 'FT':
-                            pitch_type['FT'] += 1
-                            pitch_type['total'] += 1
-                        elif pt == 'FC':
-                            pitch_type['FC'] += 1
-                            pitch_type['total'] += 1
-                        elif pt == 'SI' or pt == 'FS':
-                            pitch_type['SI'] += 1
-                            pitch_type['total'] += 1
-                        elif pt == 'SF':
-                            pitch_type['SF'] += 1
-                            pitch_type['total'] += 1
-                        elif pt == 'SL':
-                            pitch_type['SL'] += 1
-                            pitch_type['total'] += 1
-                        elif pt == 'CH':
-                            pitch_type['CH'] += 1
-                            pitch_type['total'] += 1
-                        elif pt == 'CB' or pt == 'CU':
-                            pitch_type['CB'] += 1
-                            pitch_type['total'] += 1
-                        elif pt == 'KC':
-                            pitch_type['KC'] += 1
-                            pitch_type['total'] += 1
-                        elif pt == 'KN':
-                            pitch_type['KN'] += 1
-                            pitch_type['total'] += 1
-                        elif pt == 'FT':
-                            pitch_type['EP'] += 1
-                            pitch_type['total'] += 1
+                        for poss in pitch_possibilities:
+                            for t in poss:
+                                if pt == t:
+                                    pitch_type[poss[0]] += 1
+                                    pitch_type['total'] += 1
+                        
                         
     for player, attributes in dic.items():
         pitch_type_dic = attributes['pitch_type']
@@ -120,6 +59,7 @@ def produce_results():
         pitch_type_list = ['FF','FT','FC','SI','SF','SL','CH','CB','KC','KN','EP']
         for ty in pitch_type_list:
             vector.append(pitch_type_dic[ty] / pitch_total)
+        print (player, vector)
         vector = np.array(vector, float)
         tup = (player, vector)
         results.append(tup)
