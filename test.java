705c07ec560d81b89be16c727543d6fd2574fa35
Clement Miao <clementmiao@gmail.com>
2014-05-30 18:33:21 -0500

    add libraries

--- test.java
+++ test.java
@@ -9,6 +9,8 @@ import java.io.IOException;
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.*;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.*;
