dd1c5f4bb60cc91856bddea527a7e3786eb68bd5
Clement Miao <clementmiao@gmail.com>
2014-06-01 16:54:10 -0500

    push

--- matchup.java
+++ matchup.java
@@ -211,8 +211,8 @@ public class matchup {
                                 int index = containsBatter(currentElement, batterList);                                
                                 if ( index == -1) {
                                     batter = new Batter();
-                                    // int size = clusters.size() * 2;
-                                    // batter.initArray(size);
+                                    int size = clusters.size() * 2;
+                                    batter.initArray(size);
                                     batter.setBatter(currentElement);
                                     batterList.add(batter);
                                 } else {
