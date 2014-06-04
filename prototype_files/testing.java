8715c54e704bd3d5acb1d2c062a68b0e49bc6bd3
clementmiao <clementmiao@gmail.com>
2014-06-04 02:46:32 -0500

    some stuff

---
+++ prototype_files/testing.java
@@ -0,0 +1,12 @@
+public class testing {
+    public static void main(String[] args) throws Exception {
+
+        String current_element = "";
+        if (current_element.equals("")) {
+            current_element = "WORKED";
+        }
+        System.out.println(current_element);
+
+    }    
+}
+
---
+++ prototype_files/xml2jsonScript.py
@@ -0,0 +1,21 @@
+from xml2json import xml2json
+import os
+import sys
+import glob
+import subprocess
+#from sets import Set
+
+fdepth = glob.glob('*/*/*/*/*/*')
+ddepth = filter(lambda f: os.path.isdir(f), fdepth)
+i = 0
+sdepth = set()
+for x in ddepth:
+    sdepth.add(x)
+for file in ddepth:
+    innings = os.path.join(file, "inning_all.xml")
+    print innings
+    string = 'xml2json -t xml2json -o june_data/inning_all_' + str(i) + '.json ' + innings
+    subprocess.call(string, shell=True)
+    i = i + 1
+               
+            
